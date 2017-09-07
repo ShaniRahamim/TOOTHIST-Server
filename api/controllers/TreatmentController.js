4e1100edf15e45150baa21ef6265e3eea32d7079
shani <shani@hopon.co.il>
2017-09-08 01:36:22 +0300

    getting there

--- api/controllers/TreatmentController.js
+++ api/controllers/TreatmentController.js
@@ -29,63 +29,77 @@ module.exports = {
 	
 };
 
-function voiceToText(request, response){
-    var soundBinaryStr = request.body;
+function voiceToText(req, response){
+    var soundBinaryStr = req.body;
+    try {
+        // todo: predfom a check here.
+        var textToReturn = "howdeyyy!";
+        // our Google Cloud Platform project ID
+        const projectId = 'atomic-voice-176415';
 
-    // todo: predfom a check here.
-    var textToReturn = "howdeyyy!";
-    // our Google Cloud Platform project ID
-    const projectId = 'atomic-voice-176415';
-    
         // Instantiates a client
         const speechClient = Speech({
-        projectId: projectId
+            projectId: projectId
         });
-    
+
         // The name of the audio file to transcribe
-    
+
         // Reads a local audio file and converts it to base64
         // const file = fs.readFileSync(req.body);
         let a = req.body;
-    
-        let decodedWav = wav.decode(req.body.wavFile);
+
+        // let decodedWav = wav.decode(req.body.wavFile);
+        let decodedWav = wav.decode(req.body);
+
         console.log("hi");
         var newChannelData = decodedWav.channelData; //we will change this if we find it is in stereo
         if (decodedWav.channelData.length > 1) { //if there is more than one channel, this is a stereo audio file
-        newChannelData = []; //wipe newChannelData clean
-        newChannelData.push(decodedWav.channelData[0]); //push only the first channel. If you wanted the second channel you could push [1]
+            newChannelData = []; //wipe newChannelData clean
+            newChannelData.push(decodedWav.channelData[0]); //push only the first channel. If you wanted the second channel you could push [1]
         }
         console.log(newChannelData.length);
-        var correctedAudioFile = wav.encode(newChannelData, { sampleRate: decodedWav.sampleRate, float: false, bitDepth: 16 }); //get a buffer
+        var correctedAudioFile = wav.encode(newChannelData, {
+            sampleRate: decodedWav.sampleRate,
+            float: false,
+            bitDepth: 16
+        }); //get a buffer
         const audioBytes = correctedAudioFile.toString('base64');
-    
+
         // The audio file's encoding, sample rate in hertz, and BCP-47 language code
         const audio = {
-        content: audioBytes
+            content: audioBytes
         };
         const config = {
-        encoding: 'LINEAR16',
-        sampleRateHertz: 44100,
-        languageCode: 'he-IL'
+            encoding: 'LINEAR16',
+            sampleRateHertz: 44100,
+            languageCode: 'he-IL'
         };
         const request = {
-        audio: audio,
-        config: config
+            audio: audio,
+            config: config
         };
-    
+
         // Detects speech in the audio file
         speechClient.recognize(request)
-        .then((results) => {
+            .then((results) = > {
             const transcription = results[0].results[0].alternatives[0].transcript;
-            console.log(transcription);
-            //console.log(`Transcription: ${transcription.split("").reverse().join("")}`);
-        })
-        .catch((err) => {
+        response.send(transcription);
+        console.log(transcription);
+        //console.log(`Transcription: ${transcription.split("").reverse().join("")}`);
+    })
+    .
+        catch((err) = > {
             console.error('ERROR:', err);
-        });
+    })
+        ;
         // [END speech_quickstart]
+
+       // response.send('Hello POST');
+    }
+    catch (ex){
+        response.send("shit!" + JSON.stringify(ex));
+    }
+
     
-       res.send('Hello POST');
-    
-    response.send(textToReturn);
+    //response.send(textToReturn);
 }
